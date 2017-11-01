9822486765f28b36443f1ee81c4e7594a4133432
CT-Clark <cody.t.clark@gmail.com>
2017-10-31 19:04:54 -0600

    Optimized by adding a GOTO Break
    Also removed some printing statements, left them with the option to uncomment if need be.

--- Project-Euler/Q50-Consecutive-Prime-Sum/Q50.java
+++ Project-Euler/Q50-Consecutive-Prime-Sum/Q50.java
@@ -27,8 +27,8 @@ public class Q50 {
         else if (j == (int)Math.sqrt(i)) { primes.add(i); }
       }
     }
+    System.out.println("Prime list compiled");
 
-    //
     List<Integer> temp = new ArrayList<Integer>();
     int sum = 0, maxSum = 0, count = 0, maxCount = 0;
 
@@ -40,30 +40,29 @@ public class Q50 {
      */
 
     int primeIndexResult = 0;
-    for (int primeIndex = 0; primeIndex < primes.size(); primeIndex++) {
-      for (int tempIndex = 1; tempIndex < primes.size() - primeIndex; tempIndex++) {
-        sum = 0;
-        temp = primes.subList(primeIndex, primeIndex + tempIndex);
+    for (int primeIndex = 0; primeIndex < primes.size(); primeIndex++) { // First prime of the sum
+      LAST: for (int tempIndex = 1; tempIndex < primes.size() - primeIndex; tempIndex++) { // Last prime of the sum
+              sum = 0;
+              temp = primes.subList(primeIndex, primeIndex + tempIndex); // From the first prime to the last prime
 
-        for (int i = 0; i < temp.size(); i++) { sum += temp.get(i); }
+              // Sums all of the primes in the sublist, if the sum is greater than 1000000, break, we're not interested
+              for (int i = 0; i < temp.size(); i++) { sum += temp.get(i); if (sum > 1000000) { break LAST;}}
 
-        if (sum > 1000000) { break; } // Not interested in any number if the sum is > 1000000
+              if (primes.contains(sum)) { // If the sum is a prime, compare it to past entries
+                count = temp.size();
 
-        if (primes.contains(sum)) { // If the sum is a prime, compare it to past entries
-          count = temp.size();
-
-          if (count > maxCount) { // If the sum is composed of the largest sequence to date, store it
-            maxSum = sum;
-            maxCount = count;
-            primeIndexResult = primeIndex;
+              if (count > maxCount) { // If the sum is composed of the largest sequence to date, store it
+                maxSum = sum;
+                maxCount = count;
+                primeIndexResult = primeIndex;
           }
         }
       }
-      System.out.println(temp.get(0)); // Enabling lets you check to see which index you're starting on
+      //System.out.println(temp.get(0)); // Enabling lets you check to see which index you're starting on
     }
 
-    System.out.println("maxCount: " + maxCount + " maxSum: " + maxSum);
-    System.out.println("Prime Index Number: " + primeIndexResult);
-    System.out.println(primes.subList(1, 545));
+    System.out.println("Number of Consecutive Primes: " + maxCount + " Prime Answer: " + maxSum);
+    //System.out.println("Prime Index Number: " + primeIndexResult); // Enable to see the answer's prime starting index
+    //System.out.println(primes.subList(1, 545)); // Enable to see the list of numbers that sums to the prime
   }
 }
